36c59107f7b9e51d84ffc34a420b3951b291753e
蔡三泽 <csz0102@gmail.com>
2022-04-24 17:27:39 +0800

    feat: 测试脚本

--- evaluator.py
+++ evaluator.py
@@ -18,8 +18,7 @@ class Eval_thread():
         sad = self.Eval_sad()
         mse = self.Eval_mse()
         grad = self.Eval_grad()
-        conn = 0
-        # conn = self.Eval_conn()
+        conn = self.Eval_conn()
 
         self.LOG(
             '{} ({}): {:.4f} sad || {:.4f} mse|| {:.4f} grad || {:.4f} conn\n'
@@ -75,7 +74,7 @@ class Eval_thread():
         for pred, gt in self.loader:
             predImage = numpy.array(pred)
             gtImage = numpy.array(gt)   
-            conn = utils.compute_connectivity_loss(predImage, gtImage, gtImage, 0.1)
+            conn = utils.compute_connectivity_loss(predImage, gtImage, gtImage)
             avg_conn += conn
             img_num += 1.0
         avg_conn /= img_num
